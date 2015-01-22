a4eab62ae1324e0ed79959af8ee9b19358d7f640
Pablo <pablomacveigh@gmail.com>
2015-01-22 10:26:07 +0100

    Pr7
    Practica 7 funcional. El problema de las jarras no encuentra la solución
    óptima.
    Optimización necesaria.

--- Pr7/jars.pl
+++ Pr7/jars.pl
@@ -23,5 +23,28 @@ operacion(estado(W,X,Y,Z),estado(NW,X,Y,Z)):-NW is W-W.
 %Vacía la jarra B.
 operacion(estado(W,X,Y,Z),estado(W,X,NY,Z)):-NY is Y-Y.
 
-%Vierte A en B.
-operacion(estado(W,X,Y,Z), estado(NW, X, NY, Z)):- W>0, 
+%Vierte A en B: Caso Vol A >= (Max B - Vol B)
+operacion(estado(W,X,Y,Z), estado(NW, X, NY, Z)):- AUX is Z-Y, W >= AUX, NY is Y + AUX, NW is W - AUX.
+
+%Vierte A en B: Caso Vol A < (Max B - Vol B)
+operacion(estado(W,X,Y,Z), estado(NW,X,NY,Z)):- AUX is Z-Y, W < AUX, NY is Y + W, NW is 0.
+
+%Vierte B en A: Caso Vol B >= (Max A - Vol A)
+operacion(estado(W,X,Y,Z), estado(NW,X,NY,Z)):- AUX is X-W, Y >= AUX, NW is W + AUX, NY is Y - AUX.
+
+%Vierte B en A: Caso Vol B < (Max A - Vola A)
+operacion(estado(W,X,Y,Z), estado(NW,X,NY,Z)):- AUX is X-W, Y < AUX, NW is W + Y, NY is 0.
+
+camino(estado(W1,X1,Y1,Z1),estado(W2,X2,Y2,Z2), Explorados, ListaMovimientos):-
+	operacion(estado(W1,X1,Y1,Z1),estado(W3,X3,Y3,Z3)),
+	not(member(estado(W3,X3,Y3,Z3),Explorados)),
+	camino(estado(W3,X3,Y3,Z3),estado(W2,X2,Y2,Z2),[estado(W3,X3,Y3,Z3)|Explorados],[[estado(W3,X3,Y3,Z3),estado(W1,X1,Y1,Z1)]|ListaMovimientos]).
+
+camino(estado(W,X,Y,Z),estado(W,X,Y,Z),_,ListaMovimientos):-
+	output(ListaMovimientos).
+	output([]) :- nl.
+	output([[A,B]|ListaMovimientos]) :- 
+	output(ListaMovimientos), 
+   	write(B), write(' -> '), write(A), nl.
+
+soluciona:-camino(estado(0,3,0,4),estado(_,3,2,4),estado(0,3,0,4),_).
