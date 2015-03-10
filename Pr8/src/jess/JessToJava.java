22257e26eef93c72d4e8c254e820fcddb5d9bc24
Pablo <pablomacveigh@gmail.com>
2015-03-10 22:04:28 +0100

    Pr8
    Finished, Zip added with source code and memory as pdf.

--- Pr8/src/jess/JessToJava.java
+++ Pr8/src/jess/JessToJava.java
@@ -1,3 +1,6 @@
+//Pablo Mac-Veigh
+//Manuel Sánchez
+
 package jess;
 
 import java.util.ArrayList;
@@ -15,14 +18,14 @@ public class JessToJava {
 	private static String _sourceFile;
 	private static final Scanner read = new Scanner(System.in);
 	private static final String[] MESSG = {
-		"¿En qué época del año quiere viajar?",
+		"¿En qué época del año quiere viajar? 0-Primavera, 1-Verano, 2-Otoño, 3-Invierno",
 		"¿Cuanto es su presupuesto?",
-		"¿Tiene usted alguna afición?",
+		"¿Tiene usted alguna afición? 0-Deportes, 1-Aventura,\n2-Cultura, 3-Turismo, 4-Gastronomía",
 		"¿Qué edad tiene usted?",
-		"¿Viajará acompañado?",
-		"¿Quiere un viaje exótico?",
-		"¿De qué continente procede?",
-		"¿Qué idioma habla?"
+		"¿Viajará acompañado? 0-Sólo, 1-Familia, 2-Amigos",
+		"¿Quiere un viaje exótico? 0-No, 1-Si",
+		"¿De qué continente procede? 0-Europa, 1-Asia, 2-Australia,\n3-America del Norte, 4-America del Sur",
+		"¿Qué idioma habla 0-Esp, 1-Eng, 2-Fra, 3-Chn, 4-Ger, 5-Ind, 6-Por?"
 	};
 	private static final String[] NOMVALUE ={
 		"season",
@@ -52,7 +55,6 @@ public class JessToJava {
 	public static void loadSource(String name){
 		try{
 			Value v = _rete.batch(name);
-			System.out.println("Value :"+v);
 		}catch (JessException je){
 			System.out.println("Error al cargar");
 			je.printStackTrace();
@@ -60,14 +62,14 @@ public class JessToJava {
 	}
 	
 	public static void main(String[] args) throws JessException {
-		System.out.println(args[0]);
 		JessToJava inst;
 		inst = new JessToJava();
 		treatInput(_rete);
 		reset();
 		_rete.setFocus("travelRecommendation");
 		run();
-		factList(Integer.parseInt(args[0]));
+		//factList(Integer.parseInt(args[0])); //Args should be 3 for 3 elements
+		factList(3);
 		halt();
 	}
 	
@@ -110,10 +112,15 @@ public class JessToJava {
 		Fact f = new Fact("user", r); //Se crea un nuevo fact del tipo User.
 		Value[] val = new Value[MESSG.length];
 		for(int i = 0; i<MESSG.length;i++){
+			try{
 			System.out.println(MESSG[i]);
 			userInput = read.next();
 			val[i] = new Value(Integer.parseInt(userInput));
 			f.setSlotValue(NOMVALUE[i], val[i]); //añade el Slot con nombre NOMVALUE[i] con valor val[i] al fact de tipo user.
+			}catch (Exception e){
+				System.out.println("Valor no valido, vuelva a empezar");
+				treatInput(r);
+			}
 		}
 		
 		dff.addFact(f); //Añade el fact al deffacts.
@@ -144,12 +151,12 @@ public class JessToJava {
 		List<Fact> fetchedTravels = filter("MAIN::recommendation");
 		
 		try{
-			System.out.println("Las recomendaciones de viajes son: ");
+			System.out.println("Las recomendaciones de viajes son: \n");
 			for (int i = 0; i< Math.min(maxResults,fetchedTravels.size()); i++){
 				Value name = fetchedTravels.get(i).getSlotValue("name");
 				Value description = fetchedTravels.get(i).getSlotValue("description");
 				System.out.println("Viaje: " + name.stringValue(null));
-				System.out.println("Descripción: " + description.stringValue(null));
+				System.out.println("Descripción: " + description.stringValue(null) + "\n");
 			}
 			
 			if(fetchedTravels.isEmpty())
