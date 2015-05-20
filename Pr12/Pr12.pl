875f50015cca79ba75f22d791078c9dd3e188097
Pablo <pablomacveigh@gmail.com>
2015-05-20 20:50:01 +0200

    Pr12
    -Added querries
    -Fixed grammar.
    -4 new types of sentence.
    Check the god damn log.

--- Pr12/Pr12.pl
+++ Pr12/Pr12.pl
@@ -1,29 +1,82 @@
-frase -->  grupo_anadir, grupo_quien(N), grupo_tiempo(D,M,H),
+frase(Salida) -->  grupo_anadir, grupo_quien(N), grupo_tiempo(D,M,H),
 {
 	assert(cita(D,M,H,_,N)),
-	write('Reunion programada con '),
-	write(N),
-	write(' el dia '),
-	write(D),
-	write(' de '),
-	write(M),
-	write(' a las '),
-	write(H),
-	write('.')
+
+	string_concat('Reunion programada con ', N, Aux1),
+	string_concat(Aux1, ' el dia ', Aux2),
+	string_concat(Aux2, D, Aux3),
+	string_concat(Aux3, ' de ', Aux4),
+	string_concat(Aux4, M, Aux5),
+	string_concat(Aux5, ' a las ', Aux6),
+	string_concat(Aux6, H, Aux7),
+	string_concat(Aux7, '.', Salida)
+
 }.
 
-frase -->  grupo_borrar, grupo_quien(N),
+frase(Salida) -->  grupo_borrar, grupo_quien(N),
 {
 	retract(cita(_,_,_,_,N)),
-	write('Su reunion con '),
-	write(N),
-	write(' ha sido cancelada.')
+	string_concat('Su reunion con ', N, Aux1),
+	string_concat(Aux1, ' ha sido cancelada.', Salida)
 }.
 
+frase(Salida) --> grupo_borrar, grupo_tiempo(D,M,H),
+{
+	retract(cita(D,M,H,_,_)),
+
+	string_concat('Su cita del ', D, Aux1),
+	string_concat(Aux1, '/', Aux2),
+	string_concat(Aux2, M, Aux3),
+	string_concat(Aux3, ' a las ', Aux4),
+	string_concat(Aux4, H, Aux5),
+	string_concat(Aux5, ' ha sido cancelada', Salida)
+}.
+
+frase(Salida) --> grupo_borrar, [de], grupo_tiempo(D,M,H),
+{
+	retract(cita(D,M,H,_,_)),
+
+	string_concat('Su cita del ', D, Aux1),
+	string_concat(Aux1, '/', Aux2),
+	string_concat(Aux2, M, Aux3),
+	string_concat(Aux3, ' a las ', Aux4),
+	string_concat(Aux4, H, Aux5),
+	string_concat(Aux5, ' ha sido cancelada', Salida)
+}.
+
+frase(Salida) --> grupo_querry_tiempo, grupo_tiempo(D,M,H),
+{
+%setof(cita, cita(D,M,H,_,_), Salida)
+}.
+
+frase(Salida) --> grupo_querry_persona, grupo_quien(N),
+{
+%setof(cita, cita(_,_,_,_,N), Salida)
+}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% GRAMATICAS %%%%%%%%%%%
+
+consulta:- write('Escribe frase entre corchetes separando palabras con comas '), nl,
+write('o lista vacia para parar '), nl,
+read(F),
+trata(F).
+
+trata([]):- write('final').
+trata(F):- frase(Salida, F, []), write(Salida),nl, consulta.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% GRUPOS SINTACTICOS %%%%%%
 
+%%%%% Grupo para preguntar por las citas futuras %%%%%
+
+grupo_querry_tiempo--> es_consulta_tiempo, [tengo].
+grupo_querry_tiempo--> es_consulta_tiempo, es_appointments, [tengo].
+
+%%%%% Grupo para preguntar por las citas con alguien %%%%%
+
+grupo_querry_persona--> es_consulta_persona, [tengo], es_consulta_persona_det,
+es_appointment.
 
 %%%%%% Grupo para añadir una cita
 
@@ -42,7 +95,7 @@ grupo_quien(N) --> es_prep, [N].
 
 %%%%%% Grupo temporal para dar una fecha específica.
 
-grupo_tiempo(D,M,H) --> [el], [D], [de], [M], [a,las], [H],
+grupo_tiempo(D,M,H) --> es_prep_temp, [D], [de], [M], [a,las], [H],
 {
 H>0-1,
 H<24,
@@ -60,13 +113,21 @@ hoy(D,M)
 }.
 
 
-%%%%%% Grupo temporal para mañana.
+%%%%%% Grupos temporales para mañana.
 
 grupo_tiempo(D,M,H) --> [manana,a,las], [H],
 {
 hoy(A,M),
 D is A + 1
 }.
+
+grupo_tiempo(D,M,H) --> [manana],
+{
+hoy(A,M),
+D is A + 1,
+H is 8
+}.
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -78,18 +139,29 @@ es_anadir-->[pon,una].
 es_appointment-->[reunion].
 es_appointment-->[cita].
 
+es_appointments-->[reuniones].
+es_appointments-->[citas].
+
+es_prep_temp-->[el].
+es_prep_temp-->[del].
+
 es_prep-->[de].
 es_prep-->[con].
 
 es_borrar-->[borra,la].
 es_borrar-->[quita,la].
 
-es_consulta-->[que].
-es_consulta-->[quien].
-es_consulta-->[cuando].
+es_consulta_tiempo-->[que].
+
+es_consulta_persona-->[cuando].
+
+es_consulta_persona_det-->[una].
+es_consulta_persona_det-->[la].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%% DATOS %%%%%%%%%%%%%
+:- dynamic cita/5.
+
 
 hoy(19,5).
 
